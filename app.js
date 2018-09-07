d71374448aee84664bf795d2d4afe48f27d4ae59
Hunt Applegate <huntapplegate@gmail.com>
2018-09-07 13:58:33 -0600

    added port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const planets = require("./data.js");
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.get("/", (request, response) => {
   response.send(planets);
